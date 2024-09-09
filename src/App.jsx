2895ec984d161625f2b68b844ebd8f298b6d3ea0
Jessica Manturano <jessica95.mp@gmail.com>
2024-09-08 19:36:29 -0500

    add material ui library and config navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
+import { Route, Routes } from "react-router-dom";
+import { routes } from "./utils/routes";
+import Home from "./Routes/Home";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
-
 function App() {
   return (
+    <>
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Navbar />
+          <Routes>
+            <Route path={routes.home} element={<Home />} />
+          </Routes>
+        <Footer />
       </div>
+    </>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,53 @@
 import React from 'react'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import NightlightRound from '@mui/icons-material/NightlightRound';//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import { Link } from "react-router-dom";
+import { routes } from "../utils/routes";
 
 const Navbar = () => {
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
+  <Box sx={{
+    flexGrow: 1,
+    bgcolor: 'background.default',
+    color: 'text.primary'
+    }}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to={routes.home}>
+            DH Odonto
+          </Link>
+        </Typography>
+
+        <div>
+        <Link to={routes.home}>
+          <Button color="inherit">Home</Button>
+        </Link>
+        <Link to={routes.contact}>
+          <Button color="inherit">Contact</Button>
+        </Link>
+        <Link to={routes.fav}>
+          <Button color="inherit">Fav</Button>
+        </Link>
+        <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <NightlightRound />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
 
